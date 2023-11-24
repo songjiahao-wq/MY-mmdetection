e24ffa033577a4f815b0b858b55a6cf1c76d0521
宋家豪 <1109790848@qq.com>
2023-11-24 20:54:29 +0800

    1

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import subprocess
 
 if __name__ =='__main__':
     # subprocess.run(['python', 'setup.py', 'install'])
-    subprocess.run(['python', './tools/train.py', 'configs/dino/dino-4scale_r50_8xb2-12e_coco.py']) # 训练
+    subprocess.run(['python', './tools/train.py', 'configs/detr/detr_r50_8xb2-150e_coco.py']) # 训练
     # subprocess.run(['python', './tools/analysis_tools/get_flops.py', 'configs/detr/detr_r18_8xb2-500e_coco.py',
     #                 '--cfg-options','scale=(1, 3, 640, 640) ']) # 计算参数量和计算量
     # python tools/analysis_tools/get_flops.py ${CONFIG_FILE} [--shape ${INPUT_SHAPE}]
@@ -11,6 +11,6 @@ if __name__ =='__main__':
 # 计算fps
 python  tools/analysis_tools/benchmark.py ^
        work_dirs/dino-4scale_r50_8xb2-12e_coco/dino-4scale_r50_8xb2-12e_coco.py ^
-       --checkpoint work_dirs/dino-4scale_r50_8xb2-12e_coco/epoch_1.pth ^
+       --checkpoint work_dirs/dino-4scale_r50_8xb2-12e_coco/best_coco_bbox_mAP_epoch_10.pth ^
        --launcher none
 """
